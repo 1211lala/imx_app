40b38823cea48fbe7158f294428fdf766dcb5007
1211lala <2494210546@qq.com>
2024-08-01 17:29:53 +0800

    自己系统

--- linux_c/pthread/build/CMakeFiles/main.dir/cmake_clean.cmake
+++ linux_c/pthread/build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -1,10 +1,16 @@
 file(REMOVE_RECURSE
   "CMakeFiles/main.dir/src/gpio.c.o"
+  "CMakeFiles/main.dir/src/gpio.c.o.d"
   "CMakeFiles/main.dir/src/lcd.c.o"
+  "CMakeFiles/main.dir/src/lcd.c.o.d"
   "CMakeFiles/main.dir/src/lcd_fun.c.o"
+  "CMakeFiles/main.dir/src/lcd_fun.c.o.d"
   "CMakeFiles/main.dir/src/led.c.o"
+  "CMakeFiles/main.dir/src/led.c.o.d"
   "CMakeFiles/main.dir/src/main.c.o"
+  "CMakeFiles/main.dir/src/main.c.o.d"
   "CMakeFiles/main.dir/src/uart.c.o"
+  "CMakeFiles/main.dir/src/uart.c.o.d"
   "bin/main"
   "bin/main.pdb"
 )
