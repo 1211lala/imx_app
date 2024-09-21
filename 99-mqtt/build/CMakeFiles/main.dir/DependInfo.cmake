9ada9669a6c15944491818430e5d322cc880e65c
liuao <2494210546@qq.com>
2024-09-21 23:28:38 +0800

    commit

---
+++ 99-mqtt/build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/liuao/imx6ull/user_uboot_linux_rootfs/user_rootfs/rootfs/imx_app/99-mqtt/src/main.c" "/home/liuao/imx6ull/user_uboot_linux_rootfs/user_rootfs/rootfs/imx_app/99-mqtt/build/CMakeFiles/main.dir/src/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "/home/liuao/imx6ull/user_uboot_linux_rootfs/third_lib/tslib_1.21/tslib/include"
+  "/home/liuao/imx6ull/user_uboot_linux_rootfs/third_lib/jpeg/include"
+  "/home/liuao/imx6ull/user_uboot_linux_rootfs/third_lib/zlib/include"
+  "/home/liuao/imx6ull/user_uboot_linux_rootfs/third_lib/libpng/include"
+  "/home/liuao/imx6ull/user_uboot_linux_rootfs/third_lib/freetype/include/freetype2"
+  "/home/liuao/imx6ull/user_uboot_linux_rootfs/third_lib/pahomqtt/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
